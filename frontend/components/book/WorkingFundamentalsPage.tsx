a858de222f4675a27b9fe631948ae7645d3ce503
arjayofficial127 <arjayofficial127@gmail.com>
2026-04-01 04:14:44 +0800

    Nice Book Feel Harry Potter Gryffindor

--- frontend/components/book/WorkingFundamentalsPage.tsx
+++ frontend/components/book/WorkingFundamentalsPage.tsx
@@ -22,7 +22,7 @@ export function WorkingFundamentalsPage() {
               href="/working-fundamentals-introduction"
               className={styles.startReading}
             >
-              Read the Preface
+              Begin the Preface
             </Link>
           </section>
 
